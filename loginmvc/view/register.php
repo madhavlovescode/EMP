3de9363010c7a44bf45d984baf92d366f196cfb2
Madhav Vyas <vmredmi45@gmail.com>
2025-07-14 15:13:22 +0530

    Update register.php

--- loginmvc/view/register.php
+++ loginmvc/view/register.php
@@ -6,7 +6,7 @@
     <form action="" method="POST">
         name<input type="text" name="name">
         username<input type="text" name="username" >
-        password<input type="text" name="password" >
+        password<input type="password" name="password" >
         <input type="submit" name="submit">
     </form>
 <a href="./index.php">back to login</a>
